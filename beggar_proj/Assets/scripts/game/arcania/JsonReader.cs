ae53a5c0739842b50eb5effb0eab8f7093a19d33
Pedro <forworksfr@hotmail.com>
2024-07-24 08:05:29 +0900

    - GOAL: have a list of resources read from a resource json

--- beggar_proj/Assets/scripts/game/arcania/JsonReader.cs
+++ beggar_proj/Assets/scripts/game/arcania/JsonReader.cs
@@ -4,9 +4,16 @@ using UnityEngine;
 
 public class JsonReader
 {
+    public class BasicUnit
+    {
+        public string Id;
+        public string Desc;
+        public int Max;
+        public string name;
+    }
     public class ArcaniaDatas 
-    { 
-
+    {
+        public List<BasicUnit> resources = new();
     }
     public static void ReadJson(string json, ArcaniaDatas arcaniaDatas) 
     {
@@ -16,10 +23,32 @@ public class JsonReader
         var isResource = type == "resource";
         foreach (var item in items.AsArray.Children)
         {
+            BasicUnit basicInfo = ReadBasicUnit(item);
+            arcaniaDatas.resources.Add(basicInfo);
             SimpleJSON.JSONNode id = item["id"];
             Debug.Log(id);
 
         }
 
+    }
+
+    private static BasicUnit ReadBasicUnit(SimpleJSON.JSONNode item)
+    {
+        string id = item["id"];
+        string desc = item.GetValueOrDefault("desc", null);
+        int max = item.GetValueOrDefault("max", -1);
+        var bu = new BasicUnit();
+        bu.Id = id;
+        bu.Desc = desc;
+        bu.Max = max;
+        if (item.HasKey("name"))
+        {
+            bu.name = item["name"];
+        }
+        else 
+        {
+            bu.name = char.ToUpper(id[0]) + id.Substring(1);
+        }
+        return bu;
     }
 }
