d2cbb2efc869a13f61b06d32822868f15c6c0d91
Pedro <forworksfr@hotmail.com>
2024-07-27 09:13:48 +0900

    - create task control unit to actually manage the expandable button and the lists of TTVs (perhaps have an auto list of TTV groups)

--- beggar_proj/Assets/scripts/game/MainGameControl.cs
+++ beggar_proj/Assets/scripts/game/MainGameControl.cs
@@ -1,3 +1,4 @@
+using HeartUnity;
 using System.Collections;
 using System.Collections.Generic;
 using TMPro;
@@ -8,7 +9,7 @@ public class MainGameControl : MonoBehaviour
 
     public TextAsset ResourceJson;
     private DynamicCanvas dynamicCanvas;
-    public List<ButtonWithExpandable> ButtonsWithExpandables = new();
+    public List<TaskControlUnit> TaskControls = new();
     [SerializeField]
     public TMP_FontAsset Font;
     public Sprite ExpanderSprite;
@@ -29,25 +30,51 @@ public class MainGameControl : MonoBehaviour
             dynamicCanvas.children[0].AddLayoutAndParentIt(layout);
             layout.AddLayoutChildAndParentIt(bwe);
             button.SetTextRaw(item.ConfigBasic.name);
-            ButtonsWithExpandables.Add(bwe);
+            var tcu = new TaskControlUnit();
+            TaskControls.Add(tcu);
+            
             var arrayOfChanges = item.ConfigTask.Cost;
-            foreach (var cost in arrayOfChanges)
+            var rcgIndex = 0;
+            if (arrayOfChanges != null) tcu.ChangeGroups[rcgIndex] = new TaskControlUnit.ResourceChangeGroup();
+            foreach (var changeU in arrayOfChanges)
             {
                 TripleTextView ttv = CanvasMaker.CreateTripleTextView(ButtonObjectRequest);
                 layout.AddLayoutChildAndParentIt(ttv.LayoutChild);
+                tcu.ChangeGroups[rcgIndex].tripleTextViews.Add(ttv);
             }
         }
     }
 
-    public class TaskControlUnit { 
+    public class TaskControlUnit {
+        public ButtonWithExpandable bwe;
+        public AutoList<ResourceChangeGroup> ChangeGroups = new();
+        public ResourceChangeGroup CostGroup { get => ChangeGroups[0]; set => ChangeGroups[0] = value; }
+        public ResourceChangeGroup ResultGroup { get => ChangeGroups[1]; set => ChangeGroups[1] = value; }
+        public ResourceChangeGroup RunGroup { get => ChangeGroups[2]; set => ChangeGroups[2] = value; }
+        public ResourceChangeGroup EffectGroup { get => ChangeGroups[3]; set => ChangeGroups[3] = value; }
 
+        public class ResourceChangeGroup 
+        {
+            public AutoList<TripleTextView> tripleTextViews = new();
+        }
+        public void ManualUpdate() {
+            bwe.ManualUpdate();
+            foreach (var item in ChangeGroups)
+            {
+                if (item == null) continue;
+                foreach (var ttv in item.tripleTextViews)
+                {
+                    ttv.ManualUpdate();
+                }
+            }
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
         dynamicCanvas.ManualUpdate();
-        foreach (var bwe in ButtonsWithExpandables)
+        foreach (var bwe in TaskControls)
         {
             bwe.ManualUpdate();
         }
