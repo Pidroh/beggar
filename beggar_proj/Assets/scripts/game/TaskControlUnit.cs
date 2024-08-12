8f90893a7397150bc867e3dcb8501da655f75ee2
Pedro <forworksfr@hotmail.com>
2024-08-12 13:49:02 +0900

    - create general control unit class

--- beggar_proj/Assets/scripts/game/TaskControlUnit.cs
+++ beggar_proj/Assets/scripts/game/TaskControlUnit.cs
@@ -3,9 +3,8 @@ using HeartUnity.View;
 using System;
 using System.Collections.Generic;
 
-public class FurnitureControlUnit 
+public class UnitControlUnit
 {
-    public TaskControlUnit TaskControl;
 }
 
 public class TabControlUnit
@@ -18,7 +17,7 @@ public class TabControlUnit
     internal ButtonWithProgressBar SelectionButton;
 }
 
-public class ResourceControlUnit 
+public class ResourceControlUnit
 {
     public LabelWithExpandable lwe;
     public SimpleChild<UIUnit> Description { get; internal set; }
@@ -31,10 +30,10 @@ public class ResourceControlUnit
     internal void ManualUpdate()
     {
         lwe.ManualUpdate();
-        TaskControlUnit.FeedDescription(Description, Data.ConfigBasic.Desc);
+        RTControlUnit.FeedDescription(Description, Data.ConfigBasic.Desc);
         ValueText.rawText = Data.HasMax ? $"{Data.Value} / {Data.Max}" : $"{Data.Value}";
         if (!lwe.ExpandManager.Expanded) return;
-        
+
 
         foreach (var sep in Separators)
         {
@@ -43,24 +42,27 @@ public class ResourceControlUnit
     }
 }
 
-public class ModsControlUnit {
+public class ModsControlUnit
+{
     public List<TripleTextView> ModTTVs { get; internal set; } = new();
 }
 
-public class TaskControlUnit
+public class RTControlUnit
 {
     public ButtonWithExpandable bwe;
+    public LabelWithExpandable lwe;
     public AutoList<ResourceChangeGroup> ChangeGroups = new();
     public AutoList<SeparatorWithLabel> ChangeGroupSeparators = new();
     internal RuntimeUnit Data;
     public bool TaskClicked => bwe.MainButton.Clicked;
+    public UIUnit ValueText { get; internal set; }
 
     public ResourceChangeGroup CostGroup { get => ChangeGroups[0]; set => ChangeGroups[0] = value; }
     public ResourceChangeGroup ResultGroup { get => ChangeGroups[1]; set => ChangeGroups[1] = value; }
     public ResourceChangeGroup RunGroup { get => ChangeGroups[2]; set => ChangeGroups[2] = value; }
     public ResourceChangeGroup EffectGroup { get => ChangeGroups[3]; set => ChangeGroups[3] = value; }
     public SimpleChild<UIUnit> Description { get; internal set; }
-    
+
     public Gauge XPGauge { get; internal set; }
     public SimpleChild<UIUnit> MainTitle { get; internal set; }
     public UIUnit SkillLevelText { get; internal set; }
@@ -70,20 +72,22 @@ public class TaskControlUnit
 
     public ModsControlUnit ModsUnit = new();
 
-    public class ResourceChangeGroup
-    {
-        public AutoList<TripleTextView> tripleTextViews = new();
-    }
+    public bool IsExpanded => (bwe != null && bwe.Expanded) || (lwe != null && lwe.ExpandButton);
+
     public void ManualUpdate()
     {
-        bwe.ManualUpdate();
-        bwe.ButtonProgressBar.SetProgress(Data.TaskProgressRatio);
-        bwe.MainButton.LongPressMulticlickEnabled = Data.IsInstant();
-        if (SecondaryButton != null) 
+        if (bwe != null)
+        {
+            bwe.ManualUpdate();
+            bwe.ButtonProgressBar.SetProgress(Data.TaskProgressRatio);
+            bwe.MainButton.LongPressMulticlickEnabled = Data.IsInstant();
+        }
+
+        if (SecondaryButton != null)
         {
             SecondaryButton.LayoutChild.RectTransform.SetHeight(bwe.MainButton.RectTransform.GetHeight());
             SecondaryButton.ManualUpdate();
-        } 
+        }
         if (Data.ConfigBasic.UnitType == UnitType.SKILL)
         {
             MainTitle.Element.SetTextRaw(Data.Name);
@@ -95,7 +99,11 @@ public class TaskControlUnit
             SkillLevelText.rawText = $"Lvl: {Data.Value} / {Data.Max}";
             bwe.ButtonProgressBar.Button.rawText = Data.Skill.Acquired ? "Practice skill" : "Acquire Skill";
         }
-        if (!bwe.Expanded) return;
+        if (ValueText != null)
+        {
+            ValueText.rawText = Data.HasMax ? $"{Data.Value} / {Data.Max}" : $"{Data.Value}";
+        }
+        if (!IsExpanded) return;
         SimpleChild<UIUnit> description = Description;
         string desc = Data.ConfigBasic.Desc;
         FeedDescription(description, desc);
@@ -138,6 +146,7 @@ public class TaskControlUnit
                 ttv.ManualUpdate();
             }
         }
+
     }
 
     public static void FeedDescription(SimpleChild<UIUnit> description, string desc)
@@ -147,5 +156,103 @@ public class TaskControlUnit
         description.Element.SetTextRaw(desc);
         description.ManualUpdate();
     }
+}
+
+public class ResourceChangeGroup
+{
+    public AutoList<TripleTextView> tripleTextViews = new();
+}
+
+public class TaskControlUnit
+{
+    public ButtonWithExpandable bwe;
+    public AutoList<ResourceChangeGroup> ChangeGroups = new();
+    public AutoList<SeparatorWithLabel> ChangeGroupSeparators = new();
+    internal RuntimeUnit Data;
+    public bool TaskClicked => bwe.MainButton.Clicked;
+
+    public ResourceChangeGroup CostGroup { get => ChangeGroups[0]; set => ChangeGroups[0] = value; }
+    public ResourceChangeGroup ResultGroup { get => ChangeGroups[1]; set => ChangeGroups[1] = value; }
+    public ResourceChangeGroup RunGroup { get => ChangeGroups[2]; set => ChangeGroups[2] = value; }
+    public ResourceChangeGroup EffectGroup { get => ChangeGroups[3]; set => ChangeGroups[3] = value; }
+    public SimpleChild<UIUnit> Description { get; internal set; }
+
+    public Gauge XPGauge { get; internal set; }
+    public SimpleChild<UIUnit> MainTitle { get; internal set; }
+    public UIUnit SkillLevelText { get; internal set; }
+    public SimpleChild<UIUnit> SecondaryButton { get; internal set; }
+
+    public List<SeparatorWithLabel> Separators = new();
+
+    public ModsControlUnit ModsUnit = new();
+
+
+    public void ManualUpdate()
+    {
+        bwe.ManualUpdate();
+        bwe.ButtonProgressBar.SetProgress(Data.TaskProgressRatio);
+        bwe.MainButton.LongPressMulticlickEnabled = Data.IsInstant();
+        if (SecondaryButton != null)
+        {
+            SecondaryButton.LayoutChild.RectTransform.SetHeight(bwe.MainButton.RectTransform.GetHeight());
+            SecondaryButton.ManualUpdate();
+        }
+        if (Data.ConfigBasic.UnitType == UnitType.SKILL)
+        {
+            MainTitle.Element.SetTextRaw(Data.Name);
+            MainTitle.LayoutChild.RectTransform.SetHeight(MainTitle.Element.text.preferredHeight + 20);
+            MainTitle.ManualUpdate();
+            XPGauge.SetRatio(Data.Skill.XPRatio);
+            XPGauge.layoutChild.Visible = Data.Skill.Acquired;
+            SkillLevelText.Active = Data.Skill.Acquired;
+            SkillLevelText.rawText = $"Lvl: {Data.Value} / {Data.Max}";
+            bwe.ButtonProgressBar.Button.rawText = Data.Skill.Acquired ? "Practice skill" : "Acquire Skill";
+        }
+        if (!bwe.Expanded) return;
+        SimpleChild<UIUnit> description = Description;
+        string desc = Data.ConfigBasic.Desc;
+        RTControlUnit.FeedDescription(description, desc);
+        foreach (var sep in Separators)
+        {
+            sep.ManualUpdate();
+        }
+
+        for (int i = 0; i < ChangeGroups.Count; i++)
+        {
+
+            var sep = ChangeGroupSeparators[i];
+
+            ResourceChangeGroup item = ChangeGroups[i];
+            var resourceChanges = Data.ConfigTask.GetResourceChangeList(i);
+            if (item == null || (Data.Skill != null && Data.Skill.Acquired && i == (int)ResourceChangeType.COST))
+            {
+                if (sep != null) sep.LayoutChild.Visible = false;
+                for (int ttvIndex = 0; ttvIndex < item.tripleTextViews.Count; ttvIndex++)
+                {
+                    TripleTextView ttv = item.tripleTextViews[ttvIndex];
+                    ttv.LayoutChild.Visible = false;
+                }
+                continue;
+            }
+
+
+            if (sep != null) sep.ManualUpdate();
+            sep.LayoutChild.Visible = resourceChanges.Count > 0;
+
+            for (int ttvIndex = 0; ttvIndex < item.tripleTextViews.Count; ttvIndex++)
+            {
+                TripleTextView ttv = item.tripleTextViews[ttvIndex];
+                var rc = resourceChanges[ttvIndex];
+
+                RuntimeUnit ru = rc.IdPointer.RuntimeUnit;
+                ttv.MainText.SetTextRaw(ru.Name);
+                ttv.SecondaryText.SetTextRaw($"{rc.valueChange}");
+                ttv.TertiaryText.SetTextRaw($"({ru.Value} / {ru.Max})");
+                ttv.ManualUpdate();
+            }
+        }
+    }
+
+
 }
 
