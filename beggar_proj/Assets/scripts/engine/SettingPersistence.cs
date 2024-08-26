c362fb70cf1d63b93ce4be09dbe35795a61e6c21
Pedro <forworksfr@hotmail.com>
2024-08-26 14:43:22 +0900

    - bring up play time control roughly with errors

--- beggar_proj/Assets/scripts/engine/SettingPersistence.cs
+++ beggar_proj/Assets/scripts/engine/SettingPersistence.cs
@@ -3,22 +3,64 @@
 using HeartUnity.View;
 using System;
 using System.Collections.Generic;
+using UnityEngine;
 using static HeartUnity.SettingModel;
 
 namespace HeartUnity
 {
+    [Serializable]
+    public class CommonPlayerSaveData 
+    {
+        public int TotalPlayTimeSeconds;
+    }
 
-    public class SettingPersistence {
+    public class PlayTimeControl
+    {
+        public static DateTime? dateTimePreviousScene;
+        public int PlayTimeToShow => Mathf.CeilToInt(playTime);
+        public float playTime;
+        public void Update()
+        {
+            playTime += Time.unscaledDeltaTime;
+        }
+
+        internal void Init(MiscPlayerStats playerMisc)
+        {
+            playTime = playerMisc.totalPlayTimeSeconds;
+            // get time between scene transitions
+            if (dateTimePreviousScene.HasValue)
+            {
+                if (dateTimePreviousScene < DateTime.Now)
+                {
+                    var secs = Mathf.Min((float)(DateTime.Now - dateTimePreviousScene.Value).TotalSeconds, 60f);
+                    playTime += Mathf.CeilToInt(secs);
+                }
+                dateTimePreviousScene = null;
+            }
+
+        }
+
+        internal void BeforeChangeScene()
+        {
+            dateTimePreviousScene = DateTime.Now;
+        }
+    }
+
+
+    public class SettingPersistence
+    {
         public SaveDataUnit<SettingPersistenceData> saveDataUnit = new("settings", true);
 
         internal void LoadMethod(List<SettingUnitRealTime> unitControls)
         {
-            if (saveDataUnit.TryLoad(out var data)){
+            if (saveDataUnit.TryLoad(out var data))
+            {
                 foreach (var uc in unitControls)
                 {
                     foreach (var su in data.settingUnits)
                     {
-                        if(uc.settingData.id == su.id){
+                        if (uc.settingData.id == su.id)
+                        {
                             uc.rtBool = su.dataB;
                             uc.rtInt = su.dataI;
                             uc.rtFloat = su.dataF;
@@ -47,12 +89,14 @@ namespace HeartUnity
     }
 
     [Serializable]
-    public class SettingPersistenceData {
+    public class SettingPersistenceData
+    {
         public List<SettingPersistenceUnit> settingUnits = new List<SettingPersistenceUnit>();
     }
 
     [Serializable]
-    public class SettingPersistenceUnit {
+    public class SettingPersistenceUnit
+    {
         public string id;
         public bool dataB;
         public float dataF;
