cf12ee611bcd80fc56ba372bbaccf97df2418e5a
Pedro <forworksfr@hotmail.com>
2024-08-27 13:23:55 +0900

    - bring up time show method

--- beggar_proj/Assets/scripts/engine/PlayTimeControl.cs
+++ beggar_proj/Assets/scripts/engine/PlayTimeControl.cs
@@ -35,5 +35,14 @@ namespace HeartUnity
         {
             dateTimePreviousScene = DateTime.Now;
         }
+
+        static string ConvertSecondsToTimeFormat(int totalSeconds)
+        {
+            int hours = totalSeconds / 3600;
+            int minutes = (totalSeconds % 3600) / 60;
+            int seconds = totalSeconds % 60;
+            return $"{hours:D2}:{minutes:D2}:{seconds:D2}";
+        }
+
     }
 }
