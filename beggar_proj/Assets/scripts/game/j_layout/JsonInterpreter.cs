3f9aa5fc9408547220e49f01f5e5a3780f3ad13c
Pedro <forworksfr@hotmail.com>
2025-03-05 09:59:14 +0900

    - json interpreter class

---
+++ beggar_proj/Assets/scripts/game/j_layout/JsonInterpreter.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+namespace JLayout {
+    public class JsonInterpreter
+    {
+        public static void ReadJson(string rawJsonText, LayoutMaster layoutMaster) { 
+        }
+    }
+
+    public class LayoutMaster 
+    { 
+
+    }
+
+    public class LayoutUnit 
+    { 
+
+    }
+}
