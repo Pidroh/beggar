ee8022c8b5ac707af31a7c45533fb821a7018855
Pedro <forworksfr@hotmail.com>
2025-05-01 23:54:41 +0900

    - tweak ending text to not have INTERMEDIARY ENDING and have something else (move the GAME CLEAR there or something)

--- beggar_proj/Assets/scripts/game/JGameControlExecuterEnding.cs
+++ beggar_proj/Assets/scripts/game/JGameControlExecuterEnding.cs
@@ -8,7 +8,7 @@ public static class JGameControlExecuterEnding
     public static string[] endingUnitIds = new string[ENDING_COUNT] { "ponderexistence", "ponderhappiness", "ponderreligion" };
     public static string[] endingPrefix = new string[ENDING_COUNT] { "You have become one with existence", "You are seeking happiness with your cats", "You are closer to the Divine" };
     public static string[] endingMessageSnippet = new string[ENDING_COUNT] { "I'm the beggar's journey", "The beggar's journey is the cat", "The beggar's journey is in the scriptures" };
-    public static string endingMessage = "GAME CLEARED \n$PART1$. \n At least until more content is added. \n\n Let me know you finished the game by sending me: \"$PART2$\".\n\n\n You can comment on the Reddit post, email, the Discord channel, etc";
+    public static string endingMessage = "$PART1$. \n At least until more content is added. \n\n Let me know you finished the game by sending me: \"$PART2$\".\n\n You can comment on the Reddit post, email, the Discord channel, etc";
     
     
     internal static void ManualUpdate(MainGameControl mgc, JGameControlDataHolder controlData, float dt)
@@ -35,7 +35,7 @@ public static class JGameControlExecuterEnding
 
             var message = endingMessage;
             message = message.Replace("$PART1$", endingPrefix[i]).Replace("$PART2$", endingMessageSnippet[i]);
-            controlData.EndingLayout.LayoutRU.SetTextRaw(0, "Intermediary Ending");
+            controlData.EndingLayout.LayoutRU.SetTextRaw(0, "GAME CLEARED");
             controlData.EndingLayout.LayoutRU.SetTextRaw(1, message);
             controlData.EndingLayout.LayoutRU.SetVisibleSelf(true);
             return;
