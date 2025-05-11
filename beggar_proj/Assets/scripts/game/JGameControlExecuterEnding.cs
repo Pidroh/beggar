1aacf7b66a001e1b2952bc6fd76e2d23187ca1eb
Pedro <forworksfr@hotmail.com>
2025-05-11 15:52:26 +0900

    - round up current endings and add their integration in ending control

--- beggar_proj/Assets/scripts/game/JGameControlExecuterEnding.cs
+++ beggar_proj/Assets/scripts/game/JGameControlExecuterEnding.cs
@@ -4,16 +4,19 @@
 
 public static class JGameControlExecuterEnding
 {
-    public const int ENDING_COUNT = 3;
-    public static string[] endingUnitIds = new string[ENDING_COUNT] { "ponderexistence", "ponderhappiness", "ponderreligion" };
-    public static string[] endingPrefix = new string[ENDING_COUNT] { "You have become one with existence", "You are seeking happiness with your cats", "You are closer to the Divine" };
-    public static string[] endingMessageSnippet = new string[ENDING_COUNT] { "I'm the beggar's journey", "The beggar's journey is the cat", "The beggar's journey is in the scriptures" };
+    public const int ENDING_COUNT = 5;
+    public static string[] endingUnitIds = new string[ENDING_COUNT] { "ponderexistence", "ponderhappiness", "ponderreligion", "pondergood", "fire_ending" };
+    public static string[] endingPrefix = new string[ENDING_COUNT] { "You have become one with existence", "You are seeking happiness with your cats", "You are closer to the Divine",
+        "You are maybe a better person?", "You have become one with the fire"
+    };
+    public static string[] endingMessageSnippet = new string[ENDING_COUNT] { "I'm the beggar's journey", "The beggar's journey is the cat", 
+        "The beggar's journey is in the scriptures", "I'm a good person", "Burn, baby, burn" };
     public static string endingMessage = "$PART1$. \n At least until more content is added. \n\n Let me know you finished the game by sending me: \"$PART2$\".\n\n You can comment on the Reddit post, email, the Discord channel, etc";
-    
-    
+
+
     internal static void ManualUpdate(MainGameControl mgc, JGameControlDataHolder controlData, float dt)
     {
-        if (controlData.EndingData.SettingsButton.Item2.UiUnit.Clicked) 
+        if (controlData.EndingData.SettingsButton.Item2.UiUnit.Clicked)
         {
             mgc.GoToSettings();
         }
