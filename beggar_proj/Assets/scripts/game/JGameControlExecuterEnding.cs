f8907a5b89b3069fa1ceee1f9be01888beccecca
Pedro <forworksfr@hotmail.com>
2025-07-07 16:52:01 +0900

    - crime ending

--- beggar_proj/Assets/scripts/game/JGameControlExecuterEnding.cs
+++ beggar_proj/Assets/scripts/game/JGameControlExecuterEnding.cs
@@ -4,13 +4,13 @@
 
 public static class JGameControlExecuterEnding
 {
-    public const int ENDING_COUNT = 5;
-    public static string[] endingUnitIds = new string[ENDING_COUNT] { "ponderexistence", "ponderhappiness", "ponderreligion", "pondergood", "fire_ending" };
+    public const int ENDING_COUNT = 6;
+    public static string[] endingUnitIds = new string[ENDING_COUNT] { "ponderexistence", "ponderhappiness", "ponderreligion", "pondergood", "fire_ending", "pondercrime" };
     public static string[] endingPrefix = new string[ENDING_COUNT] { "You have become one with existence", "You are seeking happiness with your cats", "You are closer to the Divine",
-        "You are maybe a better person?", "You have become one with the fire"
+        "You are maybe a better person?", "You have become one with the fire", "You have lived better than most"
     };
     public static string[] endingMessageSnippet = new string[ENDING_COUNT] { "I'm the beggar's journey", "The beggar's journey is the cat", 
-        "The beggar's journey is in the scriptures", "I'm a good person", "Burn, baby, burn" };
+        "The beggar's journey is in the scriptures", "I'm a good person", "Burn, baby, burn", "MONEY, my friend M O N E Y" };
     public static string endingMessage = "$PART1$. \n At least until more content is added. \n\n Let me know you finished the game by sending me: \"$PART2$\".\n\n You can comment on the Reddit post, email, the Discord channel, etc";
 
 
@@ -48,7 +48,7 @@ public static class JGameControlExecuterEnding
 
 public class JEndingGameData
 {
-    public RuntimeUnit[] runtimeUnits = new RuntimeUnit[JGameControlExecuterEnding.ENDING_COUNT] { null, null, null, null, null };
+    public RuntimeUnit[] runtimeUnits = new RuntimeUnit[JGameControlExecuterEnding.ENDING_COUNT] { null, null, null, null, null, null };
 
     public (JLayout.JLayoutRuntimeUnit, JLayout.JLayoutChild) SettingsButton { get; internal set; }
 }
