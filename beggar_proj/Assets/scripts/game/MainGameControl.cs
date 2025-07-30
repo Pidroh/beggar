bb7314025204791ec597b12344172876dc3890bd
Pedro <forworksfr@hotmail.com>
2025-07-30 17:26:03 +0900

    - move to own files

--- beggar_proj/Assets/scripts/game/MainGameControl.cs
+++ beggar_proj/Assets/scripts/game/MainGameControl.cs
@@ -6,46 +6,6 @@ using System.Collections.Generic;
 using System.Globalization;
 using TMPro;
 using UnityEngine;
-
-public class TitleScreenRuntimeData
-{
-    public JLayCanvas TitleCanvas { get; set; }
-    public JLayoutRuntimeUnit StartGameButton { get; set; }
-    public JLayoutRuntimeUnit TitleScreenLayout { get; set; }
-}
-
-public class TitleScreenSetup
-{
-    public static void Setup(MainGameControl mgc, TitleScreenRuntimeData titleScreenData)
-    {
-        var runtime = new JLayoutRuntimeData();
-        runtime.DefaultFont = mgc.Font;
-        runtime.ImageSprites = mgc.ResourceJson.spritesForLayout;
-        runtime.CurrentColorSchemeId = 0;
-        
-        LayoutDataMaster layoutMaster = new LayoutDataMaster();
-        JsonInterpreter.ReadJson(mgc.ResourceJson.layoutJsonForTitle.text, layoutMaster);
-        runtime.LayoutMaster = layoutMaster;
-        
-        var titleCanvas = JCanvasMaker.CreateCanvas(1, mgc.CanvasRequest, null, runtime);
-        titleScreenData.TitleCanvas = titleCanvas;
-        
-        var parentLayout = titleCanvas.children[0].LayoutRuntimeUnit;
-        
-        var endingTextLayout = JCanvasMaker.CreateLayout("ending_text", runtime);
-        titleScreenData.TitleScreenLayout = endingTextLayout;
-        parentLayout.AddLayoutAsChild(endingTextLayout);
-        
-        endingTextLayout.SetTextRaw(0, "Beggar's Journey");
-        endingTextLayout.SetTextRaw(1, "");
-        
-        var buttonLayout = endingTextLayout.LayoutChildren[0].LayoutRU;
-        buttonLayout.ButtonChildren[0].Item1.SetTextRaw(0, "Start Game");
-        titleScreenData.StartGameButton = buttonLayout;
-        
-        titleCanvas.ShowOverlay();
-    }
-}
 public class MainGameControl : MonoBehaviour
 {
 
