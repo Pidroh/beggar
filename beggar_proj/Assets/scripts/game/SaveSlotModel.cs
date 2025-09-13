996c6cbcbd3e59cc3da1f109d3fbfcc4b3e43940
Pedro <forworksfr@hotmail.com>
2025-09-13 14:15:51 +0900

    - create save slot unit class file

---
+++ beggar_proj/Assets/scripts/game/SaveSlotModel.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class SaveSlotModel 
+{
+    public List<SaveSlotUnit> saveSlots = new();
+    
+    public class SaveSlotUnit 
+    {
+        public string representativeText; 
+        public DateTime lastSaveTime;
+        public int playTimeSeconds;
+    }
+
+    [Serializable]
+    public class SaveSlotPersistenceUnit
+    {
+        public string representativeText;
+        public string lastSaveTime;
+        public int playTimeSeconds;
+    }
+
+    [Serializable]
+    public class SaveSlotPersistence 
+    {
+        public List<SaveSlotPersistenceUnit> persistenceUnits = new();
+    }
+}
