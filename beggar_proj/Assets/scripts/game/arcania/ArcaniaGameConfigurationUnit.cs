e9b29bacc4dd5e98ab98522aede5b7d882fd5bc9
Pedro <forworksfr@hotmail.com>
2025-11-03 20:43:05 +0900

    - create field for jsons in config

--- beggar_proj/Assets/scripts/game/arcania/ArcaniaGameConfigurationUnit.cs
+++ beggar_proj/Assets/scripts/game/arcania/ArcaniaGameConfigurationUnit.cs
@@ -7,6 +7,7 @@ using UnityEngine;
 public class ArcaniaGameConfigurationUnit : ScriptableObject
 {
     public List<TextAsset> jsonDatas;
+    public List<TextAsset> jsonDatasWorld;
     public TextAsset layoutJson;
     public KeyedSprites spritesForLayout;
     public LocalizedTextAsset arcaniaTranslationFile;
