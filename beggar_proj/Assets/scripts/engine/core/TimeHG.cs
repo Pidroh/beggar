20994658fb2dcd00d2b6a5d186f95491eac007ac
Pedro <forworksfr@hotmail.com>
2025-12-26 08:48:03 +0900

    time stuff

--- beggar_proj/Assets/scripts/engine/core/TimeHG.cs
+++ beggar_proj/Assets/scripts/engine/core/TimeHG.cs
@@ -2,7 +2,32 @@
 
 namespace HeartUnity
 {
-    public class TimeHG 
-    { 
+    public class TimeHG
+    {
+        public float time;
+        public double timeAsDouble;
+        public float timeSinceLevelLoad;
+        public double timeSinceLevelLoadAsDouble;
+        public float deltaTime;
+        public float fixedTime;
+        public double fixedTimeAsDouble;
+        public float unscaledTime;
+        public double unscaledTimeAsDouble;
+        public float fixedUnscaledTime;
+        public double fixedUnscaledTimeAsDouble;
+        public float unscaledDeltaTime;
+        public float fixedDeltaTime;
+        public float fixedUnscaledDeltaTime;
+        public float smoothDeltaTime;
+        public float timeScale;
+        public int frameCount;
+        public int renderedFrameCount;
+        public float realtimeSinceStartup;
+        public double realtimeSinceStartupAsDouble;
+        public float maximumDeltaTime;
+        public float maximumParticleDeltaTime;
+        public float captureDeltaTime;
+        public int captureFramerate;
+        public bool inFixedTimeStep;
     }
 }
